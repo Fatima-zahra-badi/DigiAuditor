09925865d2f80189de028e72a37e33040ad7d106
Fatima-zahra-badi <fatimabadi1997@gmail.com>
2021-04-02 15:56:35 +0200

    Ajout modification Profile

---
+++ src/PageAcceuil/NavigationBar/ModifierProfile.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from '../../GestionMissions/AjoutMission/AjouterMission.module.css';
+import Button from '../../GestionMissions/Button/Button';
+const modifierProfile = (props: any) => {
+    return (
+        <body
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-150vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+            <div className="container">
+
+                <div className="container">
+
+                    <form>
+                        <fieldset>
+                            <legend style={{ textAlign: "center", color: "red", fontStyle: "italic", fontWeight: "bold" }}>Modifier Profile</legend>
+
+                            <div className="form-group">
+
+                                <input type="text" className="form-control" id="nom" placeholder="Nom et Prenom" />
+                            </div>
+                            <div className="form-group">
+
+                                <input type="email" className="form-control" id="email" placeholder="username" />
+                            </div>
+
+                            <div className="form-group">
+
+                                <input type="email" className="form-control" id="email" placeholder="Adresse email" />
+                            </div>
+                            <div className="form-group">
+
+                                <input type="text" className="form-control" id="nom" placeholder="mot de passe" />
+                            </div>
+
+                            <div className="form-group">
+
+                                <input type="email" className="form-control" id="email" placeholder="Comfirmation mot de passe" />
+                            </div>
+                            <Button btnType="Success" type="submit">Modifier</Button>
+                            <Button clicked={props.clicked} btnType="Danger">Retour</Button>
+
+
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </body>
+
+    );
+}
+
+export default modifierProfile;
--- src/PageAcceuil/NavigationBar/NavigationBar.tsx
+++ src/PageAcceuil/NavigationBar/NavigationBar.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from '../../assets/pp.jpg';
 import imageProfile from '../../assets/profileImage.jpg';
 import classes from './NavigationBar.module.css';
-const NavigationBar = () => {
-    return (
-        <div>
+import ModifierProfile from './ModifierProfile';
+class NavigationBar extends Component {
+    state = {
+        modif: false
+    }
+    FormulaireModifProfile = () => {
+        this.setState({
+            modif: true
+        });
+    }
+    CancelFormulaire = () => {
+        this.setState({
+            modif: false
+        });
+    }
+    render() {
+      return (  <div>
+
             <img src={Logo} className={classes.LogoCss} alt="logo" />
-           
-        <ul>
-           
-            <li><a className={classes.active} href="#home">Acceuil</a></li>
-            <li><a href="#news">Gestion des Missions</a></li>
-            <li><a href="#contact">Gestion utilisateurs</a></li>
-            <li><a href="#about">About</a></li>
+
+            <ul className={classes.menuPage}>
+
+                <li className={classes.lienPage}><a className={classes.active} href="#home">Acceuil</a></li>
+                <li className={classes.lienPage}><a href="#news">Gestion des Missions</a></li>
+                <li className={classes.lienPage}><a href="#contact">Gestion utilisateurs</a></li>
+                <li className={classes.lienPage}><a href="#about">About</a></li>
+
             </ul>
-            
-            <img className={classes.imagecover} src={imageProfile} alt="logo" />
-         
+            <div className={classes.dropdown}>
+                <button className={classes.dropbtn}>
+                    <img className={classes.imagecover} src={imageProfile} alt="logo" />
+                </button>
+                <div className={classes.dropdowncontent}>
+                    <button onClick={this.FormulaireModifProfile} >Mon profile</button>
+                    <button onClick={this.FormulaireModifProfile} >Deconnexion</button>
+
+                </div>
+                  F.BADI
             </div>
-    );
+          <ModifierProfile show={this.state.modif} clicked={this.CancelFormulaire}>
+          </ModifierProfile>
+        </div>
+    )}
 }
 export default NavigationBar;
